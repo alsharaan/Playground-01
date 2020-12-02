d42ea2c00ea0b89a9c37e6ae1e6a21b56a303720
alsharaan/ziyad <ziyad@alsharan.com>
2020-12-02 18:47:39 +0300

    Initial Commit

---
+++ MyOfficeNote/MyOfficeNote/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MyOfficeNote
+//
+//  Created by Ziyad Al sharaan on 11/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("مرحبا")
+            .padding()
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ MyOfficeNote/MyOfficeNote/MyOfficeNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOfficeNoteApp.swift
+//  MyOfficeNote
+//
+//  Created by Ziyad Al sharaan on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct MyOfficeNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
